923d713abad7feef91c76e367a9d757e9f5f1e33
Усач Данил <danya228v@yandex.ru>
2024-04-04 09:26:58 +0300

    add branch1

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
 Console.WriteLine("start1");
+Console.WriteLine("branch1");
+Console.WriteLine("start1");
 Console.WriteLine("part1");
